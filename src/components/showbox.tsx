647d5af81db22a299604faeca8b11d9ed779af90
Carl Blancaflor <blancaflorcarlferros@gmail.com>
2025-07-25 22:45:51 +0800

    style: update CSS with Tailwind integration and component adjustments

--- src/components/showbox.tsx
+++ src/components/showbox.tsx
@@ -1,5 +1,7 @@
 function Showbox() {
-  return <div className="showbox">gdbgfgfbfhgfhgffg</div>;
+  return (
+    <div className="h-[500px] flex:col transition bg-black text-white p-4 rounded-xxl shadow pad m-5"></div>
+  );
 }
 
 export default Showbox;
